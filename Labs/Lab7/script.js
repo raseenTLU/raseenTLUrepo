39e9f2f89ec922747c7f685b4aa0662d7f508142
raseenTLU <raseena@tlu.ee>
2025-10-01 14:40:07 +0300

    last week

---
+++ Labs/Lab7/script.js
@@ -0,0 +1,20 @@
+let fname = "Raseen";
+console.log("hi there");
+console.log(fname);
+let intro = "hello the name is.." + fname;
+let sent = "hello i am " + fname + ". i am called "+fname+". call me "+fname+ ". never something other than "+fname+".";
+console.log(sent)
+let lname = "Akhtar"
+let sentLast = `hello i am ${lname}. i am called ${lname}. call me ${lname}. never something other than ${lname}.`;
+console.log(sentLast);
+console.log(intro);
+let mixed = fname + " " + 2;
+console.log(mixed);
+// const num2;
+
+let a = 9; let b = 2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b); // even or odd, every nth time, coin distribution problem
